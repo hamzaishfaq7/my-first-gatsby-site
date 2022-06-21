569603dea5074c14bd8cdc040da7e08c896b8809
hamzaishfaq7 <hamzaishfaq980@gmail.com>
2022-06-21 04:55:13 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Hamza's Portfolio">
+      <p>I'm making this for my portfolio using Gatsby. Here is a pic of poptart</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        alt="POP"
         src="../images/pop.jpg"
       />
     </Layout>
